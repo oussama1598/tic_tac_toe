885252394abd9930467e2cba9836c32ef552473d
Oussama Barkouki <oussama1598@gmail.com>
2020-01-17 04:54:48 +0100

    Fixed a return type conflict within the ai player

--- src/modules/minimax_alphabeta_ai_player/minimax_alphabeta_ai_player.h
+++ src/modules/minimax_alphabeta_ai_player/minimax_alphabeta_ai_player.h
@@ -29,6 +29,6 @@ int game_matrix_copy[3][3];
 int check_win_in_board();
 minimax_data min_alpha_beta(int alpha, int beta);
 minimax_data max_alpha_beta(int alpha, int beta);
-int minimax_alphabeta_ai_play_next_move();
+void minimax_alphabeta_ai_play_next_move();
 
 #endif
